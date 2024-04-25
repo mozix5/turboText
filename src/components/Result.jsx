41539c1451c185d283871709fd777c3585365113
mozix5 <mosinmd827@gmail.com>
2024-04-26 03:26:16 +0530

    changed layout

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,16 +1,31 @@
 import React from "react";
+import { motion } from "framer-motion";
 
-const Result = ({ userChar, textToWrite, errors,accuracy,wpm }) => {
-  
+const Result = ({ userChar, textToWrite, errors, accuracy, wpm }) => {
   return (
-    <div className="flex-1 text-caret flex items-center text-2xl justify-center flex-col">
-      <div>Result </div>
-      <div>Total Errors: {errors}</div>
-      <div>Accuracy: {accuracy}%</div>
-      <div>words per minute: {wpm}%</div>
-      <div>char: {userChar}%</div>
-      <div>error: {errors}%</div>
-    </div>
+    <motion.div
+      className="flex-1 text-caret flex items-center text-2xl justify-center gap-10"
+      initial={{ y: 400 }}
+      animate={{ y: 0 }}
+    >
+      {/* <div>Result </div> */}
+      <div className=" flex gap-2">
+        <div className=" text-textSecondary">acc:</div>
+        <div className="font-bold">{accuracy.toFixed(0)}%</div>
+      </div>
+      <div className=" flex gap-2">
+        <div className=" text-textSecondary">wpm: </div>
+        <div className=" font-bold">{wpm}</div>
+      </div>
+      <div className=" flex gap-2">
+        <div className=" text-textSecondary">char: </div>
+        <div className=" font-bold">{userChar}</div>
+      </div>
+      <div className=" flex gap-2">
+        <div className=" text-textSecondary">error: </div>
+        <div className="text-error font-bold">{errors}</div>
+      </div>
+    </motion.div>
   );
 };
 
