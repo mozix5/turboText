2e4a1059fddf9e7ae160f54949fab120ba05c2fb
mozix5 <mosinmd827@gmail.com>
2024-04-26 03:25:11 +0530

    add layout method

--- src/components/Caret.jsx
+++ src/components/Caret.jsx
@@ -3,13 +3,9 @@ import { motion } from "framer-motion";
 const Caret = ({ repeatValue }) => {
   return (
     <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 1 }}
+      layout
       transition={{
-        repeat: repeatValue,
-        duration: 1,
-        ease: "easeInOut",
+        duration: 0.1,
       }}
       className=" w-[2px] leading-relaxed h-8 bg-caret"
     ></motion.div>
