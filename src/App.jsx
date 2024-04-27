0c483f00bbb2ef4d10dd8a54779d7f371be819ae
mozix5 <mosinmd827@gmail.com>
2024-04-27 22:39:36 +0530

    added a screen when keypress looses focus

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import GenerateWords from "./components/GenerateWords";
 import InputText from "./components/InputText";
 import Navbar from "./components/Navbar";
@@ -7,16 +7,20 @@ import { VscDebugRestart } from "react-icons/vsc";
 import { faker } from "@faker-js/faker";
 import Result from "./components/Result";
 import { countErrors, getAccuracy, wpm } from "./utils/helpers/helpers";
+import { AnimatePresence } from "framer-motion";
 
 export const App = () => {
   const [userInput, setUserInput] = useState([]);
-  const [timer, setTimer] = useState(10);
+  const [timer, setTimer] = useState(15);
   const [flag, setFlag] = useState(false);
   const [isFinish, setIsFinish] = useState(false);
   const [words, setWords] = useState("");
+  const [isFocused, setIsFocused] = useState(true);
+  const inputRef = useRef();
 
   useEffect(() => {
-    setWords(faker.word.words(20));
+    setWords(faker.word.words(30));
+    inputRef.current.focus();
   }, []);
 
   // console.log(words);
@@ -42,62 +46,95 @@ export const App = () => {
 
   const handleKeyPress = (e) => {
     const char = e.key;
-    if (e.keyCode === 8) {
+    // console.log(e);
+    if (e.key === "Backspace") {
       if (userInput.length > 0) {
-        setUserInput((prevInput) => prevInput.slice(0, -1));
+        return e.ctrlKey
+          ? setUserInput((prevInput) =>
+              prevInput.slice(0, prevInput.lastIndexOf(" ") + 1)
+            )
+          : setUserInput((prevInput) => prevInput.slice(0, -1));
       }
-    } else {
-      setFlag(true);
+    } else if (e.key == " ") {
       setUserInput((prevInput) => [...prevInput, char]);
+    } else if (e.keyCode >= 65 && e.keyCode <= 90) {
+      setFlag(true);
+      return e.shiftKey
+        ? setUserInput((prevInput) => [...prevInput, char.toUpperCase()])
+        : setUserInput((prevInput) => [...prevInput, char]);
     }
   };
 
   const restartGame = () => {
-    setTimer(10);
+    setTimer(15);
     setUserInput([]);
     setFlag(false);
-    setIsFinish(false)
+    setIsFinish(false);
+    setIsFocused(false);
+    // inputRef.current.focus();
   };
 
+  const handleFocus = () => {
+    setIsFocused(false);
+    setFlag(false);
+  };
   return (
-    <div
-      className="h-screen w-screen overflow-hidden bg-bgPrimary font-roboto px-48 flex flex-col justify-between"
-      tabIndex="0"
-      onKeyDown={handleKeyPress}
-    >
+    <div className="h-screen w-screen overflow-hidden bg-bgPrimary font-roboto px-48 flex flex-col justify-between">
       <Navbar />
       <div className="">
-      {isFinish ? (
-        <Result
-          userChar={userInput.length}
-          textToWrite={words.split("").slice(0, userInput.length)}
-          errors={countErrors(
-            words.split("").slice(0, userInput.length),
-            userInput
+        <AnimatePresence>
+          {isFinish ? (
+            <Result
+              userChar={userInput.length}
+              textToWrite={words.split("").slice(0, userInput.length)}
+              errors={countErrors(
+                words.split("").slice(0, userInput.length),
+                userInput
+              )}
+              accuracy={getAccuracy(
+                userInput.length,
+                countErrors(
+                  words.split("").slice(0, userInput.length),
+                  userInput
+                )
+              )}
+              wpm={wpm(
+                userInput.length,
+                countErrors(
+                  words.split("").slice(0, userInput.length),
+                  userInput
+                ),
+                timer
+              )}
+            />
+          ) : (
+            <div
+              className="leading-relaxed text-2xl outline-none tracking-wider flex-1 flex flex-col justify-center flex-wrap"
+              ref={inputRef}
+              onKeyDown={handleKeyPress}
+              tabIndex={0}
+              onBlur={handleFocus}
+            >
+              <div className="text-caret py-8">{timer}</div>
+              <div className="relative min-h-28 break-all">
+                <GenerateWords words={words} />
+                <InputText text={userInput} textToWrite={words} />
+                {!isFocused && (
+                  <div
+                    onClick={() => setIsFocused(true)}
+                    className="glass-container absolute top-0 left-0 right-0 bottom-0 text-gray-500 text-lg font-bold flex justify-center items-center"
+                  >
+                    Click here to focus
+                  </div>
+                )}
+              </div>
+            </div>
           )}
-          accuracy={getAccuracy(
-            userInput.length,
-            countErrors(words.split("").slice(0, userInput.length), userInput)
-          )}
-          wpm={wpm(
-            userInput.length,
-            countErrors(words.split("").slice(0, userInput.length), userInput),
-            timer
-          )}
-        />
-      ) : (
-        <div className="leading-relaxed text-2xl tracking-wider flex-1 flex flex-col justify-center flex-wrap">
-          <div className="text-caret py-8">{timer}</div>
-          <div className="relative min-h-28 break-all">
-            <GenerateWords words={words} />
-            <InputText text={userInput} textToWrite={words} />
-          </div>
-        </div>
-      )}
+        </AnimatePresence>
       </div>
-      <div className="flex justify-center py-3 bg-t">
+      <div className="flex justify-center py-3">
         <VscDebugRestart
-          className="text-typography cursor-pointer text-xl"
+          className="text-textSecondary cursor-pointer text-xl"
           onClick={restartGame}
         />
       </div>
